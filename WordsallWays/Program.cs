5aa1061b3dd62e96ab7097500d7662a031190aa0
beulah444 <beulah444@gmail.com>
2017-08-28 16:49:43 -0700

    Add project files.

---
+++ WordsallWays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordsallWays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           char[] chars= "abcdefghijklmnopqrstuvwxyz       ".ToCharArray();
+            int len = 7;
+            iterate(chars, len, new char[len], 0);
+            Console.ReadKey();
+        }
+        public static void iterate(char[] chars, int len, char[] build, int pos)
+        {
+            if (pos == len)
+            {
+                String word = new String(build);
+                //  each formed word prints here
+                Console.WriteLine(word);
+                return;
+            }
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                build[pos] = chars[i];
+                iterate(chars, len, build, pos + 1);
+            }
+        }
+    }
+}
